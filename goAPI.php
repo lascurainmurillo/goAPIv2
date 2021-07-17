efaf3c0ce8b03f057c2b7ed5bfb8734fb09d6edf
markCornejo <t_falcom@hotmail.com>
2021-07-16 20:22:54 -0500

    [Add] actualizacion

--- goAPI.php
+++ goAPI.php
@@ -63,7 +63,7 @@
 	
     $log_user     = $session_user ?? '';
     $log_group    = go_get_groupid($session_user ?? '', $astDB);    
-    $log_ip       = $astDB->escape($_REQUEST['log_ip']);
+    @$log_ip       = $astDB->escape(@$_REQUEST['log_ip']);
     $goUser       = $astDB->escape($_REQUEST['goUser']);
     $goPass       = (isset($_REQUEST['log_pass']) ? $astDB->escape($_REQUEST['log_pass']) : $astDB->escape($_REQUEST['goPass']));		
 		
